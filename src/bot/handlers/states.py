8f12607c4883ba121b83a95c3b905630bbd1ab7e
inikonoff <inikonoff@gmail.com>
2026-03-07 12:12:36 +0300

    Add files via upload

--- src/bot/handlers/states.py
+++ src/bot/handlers/states.py
@@ -9,3 +9,7 @@ from aiogram.fsm.state import State, StatesGroup
 class FlowState(StatesGroup):
     choosing_persona = State()
     active = State()
+
+
+class AdminState(StatesGroup):
+    waiting_broadcast = State()
