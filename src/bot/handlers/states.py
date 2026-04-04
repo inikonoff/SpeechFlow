499947af51b7acca39878f7369840b6a47b95d88
inikonoff <inikonoff@gmail.com>
2026-04-04 19:13:42 +0300

    Update states.py

--- src/bot/handlers/states.py
+++ src/bot/handlers/states.py
@@ -17,3 +17,12 @@ class AdminState(StatesGroup):
 
 class TutorState(StatesGroup):
     awaiting_drill = State()   # ждём повтор после коррекции
+
+
+class OnboardingState(StatesGroup):
+    waiting_level = State()    # показали приветствие, ждём выбор уровня
+    waiting_first_message = State()  # показали реакцию на уровень, ждём первое сообщение
+
+
+class LevelChangeState(StatesGroup):
+    choosing_level = State()   # пользователь нажал "Сменить уровень"
